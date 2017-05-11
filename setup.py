71318bb449804b7378e969fa6d86d34a707ce896
gregology <greg.clarke@shopify.com>
2017-05-11 17:02:53 -0400

    bumping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='biased-stop-words',
-    version='2017.5.11.1',
+    version='2017.5.11.2',
     description='Generates biased stop word lists for various genres',
     long_description=open('README.rst').read(),
     license=open('LICENSE').read(),
